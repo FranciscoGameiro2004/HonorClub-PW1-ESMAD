71e70f242afca89a4306a3c4a0d57c2a0b2e7be7
ruiiramos73 <ruiramos2004@gmail.com>
2024-01-07 17:08:08 +0000

    Add navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,76 @@
 <template>
-  <h1>Jogos em destaque</h1>
+  <div class="navbar">
+  <router-link to="/">
+    <div class="logo">
+      <img src="../assets/logo.png" alt="Logo" />
+    </div>
+  </router-link>
+
+    <div class="nav-links">
+      <router-link to="/games">Jogos</router-link>
+      <router-link to="/news">Notícias</router-link>
+      <router-link to="/rules">Regras</router-link>
+    </div>
+
+    <div class="image-text">
+      <img src="../assets/avatar.png" alt="Image"/>
+      <p class="loggedUser">Olá, {{ loggedUser }}!</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { RouterLink, RouterView } from 'vue-router'
 export default {
 
 }
 </script>
 
 <style>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #2C4130;
+  color: #D4D6E3;
+}
 
+.logo img {
+  max-width: 140px;
+  height: auto;
+  margin-left: 25px;
+  margin-top: 5px;
+}
+
+.nav-links {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  color: #D4D6E3;
+}
+
+.nav-links a {
+  margin: 0 10px;
+  color: #D4D6E3;
+}
+
+.image-text {
+  display: flex;
+  align-items: center;
+  color: #D4D6E3;
+}
+
+.image-text img {
+  max-width: 30px;
+  margin-right: 7px;
+}
+
+.loggedUser {
+  white-space: nowrap;
+  margin-right: 60px;
+}
 </style>
