9eeb6c89532ad4e3fe21a5215ded7a319f798371
ruiiramos73 <ruiramos2004@gmail.com>
2024-01-08 18:55:56 +0000

    fetch

--- src/stores/games.js
+++ src/stores/games.js
@@ -1,15 +1,25 @@
 import { defineStore } from 'pinia'
 
 export const useGameStore = defineStore('games', {
-  state: () => ({games:[], currentGameId:0}),
+  state: () => ({
+    games:[], 
+    currentGameId:0
+  }),
 getters: {
     getGames: (state) => state.games,
     getGame: (state) => gameId => state.find(game, game.id == gameId)
   },
   actions: {
     // TODO: ADICIONAR ACTIONS DE BUSCA ATRAVÉS DA API DEPOIS DE SER FEITO OS MOCKS NECESSÁRIOS
-    increment() {
-      this.count++
+    async fetchGame() {
+      try {
+        const data = await api.get(API_BASE_URL, 'game');
+        this.games = data;
+        //console.log(this.games);
+      } catch (error) {
+        console.error(`Error in store fetching objects:`, error);
+        throw error
+      }
     },
   },
 })
