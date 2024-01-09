939f91354f9902ca681c355b9d29c30605f2b4dd
ruiiramos73 <ruiramos2004@gmail.com>
2024-01-09 00:27:24 +0000

    Add mocks

---
+++ src/api/mocks/mocks.js
@@ -0,0 +1,167 @@
+// SE VIREM QUE FALTA ALGUMA INFORMAÇÃO ADICIONEM, CRIEI PARA TERMOS PELO MENOS JÁ ESTE ELEMENTO CRUCIAL SEMI-ORGANIZADO!
+export default [
+    {
+      "jogos": [
+        { 
+          "id": 1, 
+          "estado": "Terminado",
+          "equipa_A": {
+            "nome": "Benfica",
+            "icon": "src/assets/benfica.png",
+            "jogadores": ["José Manel", "Pedro Pinheiro", "Francisco Fagundes", "Afonso Bruno", "Duarte Pinheiro", "Jorge Andrade", "Bernardo Soares", "Vitor Silva", "Joaquim Neves", "Sandro Felgueiras", "Ricardo Almeida", "Carlos Soares", "João Osório", "Luis Pinho", "Paulo Vitor"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "equipa_B": {
+            "nome": "Agronomia",
+            "icon": "src/assets/agronomia.png",
+            "jogadores": ["Lucas Oliveira", "Gabriel Santos", "Mateus Silva", "Pedro Pereira", "Felipe Costa", "Enzo Rodrigues", "Miguel Almeida", "João Ferreira", "Leonardo Carvalho", "Guilherme Martins", "Daniel Araújo", "Mateus Lima", "Artur Gomes", "Rafael Vieira", "Gustavo Ramos"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "resultado": "34-34",
+          "resultado_intervalo": "20-10",
+          "data": "02-12-2023 às 18:00",
+          "local": "Bulldogs Rugby Field",
+          "resumo": {
+            "primeira_parte": {
+              "minutos": ["10", "20", "30"],
+              "jogadores": ["Luis Pinho", "Duarte Pinheiro", "Vitor Silva",],
+              "acontecimentos": ["Golo", "Falta", "Cartão Vermelho"]
+            },
+            "segunda_parte": {
+              "minutos": ["40", "50", "60"],
+              "jogadores": ["Artur Gomes", "Enzo Rodrigues", "Felipe Costa",],
+              "acontecimentos": ["Golo", "Falta", "Cartão Vermelho"]
+            },
+          }
+        },
+        { 
+          "id": 2, 
+          "estado": "Terminado",
+          "equipa_A": {
+            "nome": "CDUP",
+            "icon": "src/assets/cdup.png",
+            "jogadores": ["José Manel", "Pedro Pinheiro", "Francisco Fagundes", "Afonso Bruno", "Duarte Pinheiro", "Jorge Andrade", "Bernardo Soares", "Vitor Silva", "Joaquim Neves", "Sandro Felgueiras", "Ricardo Almeida", "Carlos Soares", "João Osório", "Luis Pinho", "Paulo Vitor"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "equipa_B": {
+            "nome": "CDUL",
+            "icon": "src/assets/cdul.png",
+            "jogadores": ["Lucas Oliveira", "Gabriel Santos", "Mateus Silva", "Pedro Pereira", "Felipe Costa", "Enzo Rodrigues", "Miguel Almeida", "João Ferreira", "Leonardo Carvalho", "Guilherme Martins", "Daniel Araújo", "Mateus Lima", "Artur Gomes", "Rafael Vieira", "Gustavo Ramos"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "resultado": "21-50",
+          "resultado_intervalo": "10-30",
+          "data": "09-12-2023 às 16:00",
+          "local": "Peacocks Rugby Field",
+          "resumo": {
+            "primeira_parte": {
+              "minutos": ["10", "20", "30"],
+              "jogadores": ["José Manel", "Francisco Fagundes", "Jorge Andrade",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+            "segunda_parte": {
+              "minutos": ["40", "50", "60"],
+              "jogadores": ["Felipe Costa", "Enzo Rodrigues", "Pedro Pereira",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+          }
+        }, 
+        { 
+          "id": 3, 
+          "estado": "Terminado",
+          "equipa_A": {
+            "nome": "Os Belenenses",
+            "icon": "src/assets/belenenses.png",
+            "jogadores": ["José Manel", "Pedro Pinheiro", "Francisco Fagundes", "Afonso Bruno", "Duarte Pinheiro", "Jorge Andrade", "Bernardo Soares", "Vitor Silva", "Joaquim Neves", "Sandro Felgueiras", "Ricardo Almeida", "Carlos Soares", "João Osório", "Luis Pinho", "Paulo Vitor"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "equipa_B": {
+            "nome": "Direito",
+            "icon": "src/assets/direito.png",
+            "jogadores": ["Lucas Oliveira", "Gabriel Santos", "Mateus Silva", "Pedro Pereira", "Felipe Costa", "Enzo Rodrigues", "Miguel Almeida", "João Ferreira", "Leonardo Carvalho", "Guilherme Martins", "Daniel Araújo", "Mateus Lima", "Artur Gomes", "Rafael Vieira", "Gustavo Ramos"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "resultado": "30-17",
+          "resultado_intervalo": "15-8",
+          "data": "09-12-2023 às 14:00",
+          "local": "Peacocks Rugby Field",
+          "resumo": {
+            "primeira_parte": {
+              "minutos": ["10", "20", "30"],
+              "jogadores": ["José Manel", "Francisco Fagundes", "Jorge Andrade",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+            "segunda_parte": {
+              "minutos": ["40", "50", "60"],
+              "jogadores": ["Felipe Costa", "Enzo Rodrigues", "Pedro Pereira",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+          }
+        },
+        { 
+          "id": 4, 
+          "estado": "Terminado",
+          "equipa_A": {
+            "nome": "Académica",
+            "icon": "src/assets/academica.png",
+            "jogadores": ["José Manel", "Pedro Pinheiro", "Francisco Fagundes", "Afonso Bruno", "Duarte Pinheiro", "Jorge Andrade", "Bernardo Soares", "Vitor Silva", "Joaquim Neves", "Sandro Felgueiras", "Ricardo Almeida", "Carlos Soares", "João Osório", "Luis Pinho", "Paulo Vitor"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "equipa_B": {
+            "nome": "São miguel",
+            "icon": "src/assets/smiguel.png",
+            "jogadores": ["Lucas Oliveira", "Gabriel Santos", "Mateus Silva", "Pedro Pereira", "Felipe Costa", "Enzo Rodrigues", "Miguel Almeida", "João Ferreira", "Leonardo Carvalho", "Guilherme Martins", "Daniel Araújo", "Mateus Lima", "Artur Gomes", "Rafael Vieira", "Gustavo Ramos"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "resultado": "38-20",
+          "resultado_intervalo": "28-17",
+          "data": "08-12-2023 às 15:00",
+          "local": "Peacocks Rugby Field",
+          "resumo": {
+            "primeira_parte": {
+              "minutos": ["10", "20", "30"],
+              "jogadores": ["José Manel", "Francisco Fagundes", "Jorge Andrade",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+            "segunda_parte": {
+              "minutos": ["40", "50", "60"],
+              "jogadores": ["Felipe Costa", "Enzo Rodrigues", "Pedro Pereira",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+          }
+        },
+        { 
+          "id": 5, 
+          "estado": "Terminado",
+          "equipa_A": {
+            "nome": "São Miguel",
+            "icon": "src/assets/smiguel.png",
+            "jogadores": ["José Manel", "Pedro Pinheiro", "Francisco Fagundes", "Afonso Bruno", "Duarte Pinheiro", "Jorge Andrade", "Bernardo Soares", "Vitor Silva", "Joaquim Neves", "Sandro Felgueiras", "Ricardo Almeida", "Carlos Soares", "João Osório", "Luis Pinho", "Paulo Vitor"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "equipa_B": {
+            "nome": "Os Belenenses",
+            "icon": "src/assets/belenenses.png",
+            "jogadores": ["Lucas Oliveira", "Gabriel Santos", "Mateus Silva", "Pedro Pereira", "Felipe Costa", "Enzo Rodrigues", "Miguel Almeida", "João Ferreira", "Leonardo Carvalho", "Guilherme Martins", "Daniel Araújo", "Mateus Lima", "Artur Gomes", "Rafael Vieira", "Gustavo Ramos"],
+            "posições": ["Pilar", "Hooker", "Pilar", "Segunda Linha", "Segunda Linha", "Asa", "Asa", "Número 8", "Half-Back", "Fly-Half", "Ponta", "Primeiro Centro", "Segundo Centro", "Ponta", "Fullback"]
+          },
+          "resultado": "10-46",
+          "resultado_intervalo": "10-18",
+          "data": "02-12-2023 às 18:00",
+          "local": "Peacocks Rugby Field",
+          "resumo": {
+            "primeira_parte": {
+              "minutos": ["10", "20", "30"],
+              "jogadores": ["José Manel", "Francisco Fagundes", "Jorge Andrade",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+            "segunda_parte": {
+              "minutos": ["40", "50", "60"],
+              "jogadores": ["Felipe Costa", "Enzo Rodrigues", "Pedro Pereira",],
+              "acontecimentos": ["Falta", "Golo", "Cartão Vermelho"]
+            },
+          }
+        },
+      ]
+    },
+];
